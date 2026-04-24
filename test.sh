9987bc6560bcd98fe3b112536509c248c5ea8830
Tiffany <Galacthfansmo0nar45@gmail.com>
2026-04-24 23:58:19 +0800

    pencarian host ip

--- test.sh
+++ test.sh
@@ -3,13 +3,10 @@ echo "Testing Gamify App..."
 sleep 30
 
 # MASUKKAN IP LXC KAMU SECARA MANUAL (Misal 192.168.1.16)
-TARGET_IP="192.168.1.16"
 
-echo "Targeting Host IP: $TARGET_IP"
 
 # Coba nembak ke login.html (pastikan nama filenya benar)
-status=$(curl -s -o /dev/null -w "%{http_code}" http://$TARGET_IP:8082/login.html)
-
+status=$(curl -s -o /dev/null -w "%{http_code}" http://172.17.0.1:8082/login.html)
 if [ $status -eq 200 ]; then
   echo "SUCCESS: App is rendering (HTTP 200)"
   exit 0
