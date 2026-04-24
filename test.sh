bc1ed409461fba390a1599bb734da9fa7c5ec3e0
Tiffany <Galacthfansmo0nar45@gmail.com>
2026-04-24 22:51:56 +0800

    Update commit: Perbaikan pada file docker compose guna perbaikan port yang bertabrakan

--- test.sh
+++ test.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 echo "Testing Gamify App..."
 # Tunggu 20 detik biar makin aman, Java/Nginx butuh waktu buat 'bangun'
-sleep 20 
+sleep 10
 
 # Ganti ke 8082 karena 8080 punyanya si Jenkins
 # Ganti localhost dengan IP Host/LXC kamu
-status=$(curl -s -o /dev/null -w "%{http_code}" http://localhost:8082)
+status=$(curl -s -o /dev/null -w "%{http_code}" http://localhost:8081)
 
 if [ $status -eq 200 ]; then
   echo "SUCCESS: App is rendering (HTTP 200)"
