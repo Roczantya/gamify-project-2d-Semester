460184eb43d453f44e603e164cfef276815350b7
Tiffany <Galacthfansmo0nar45@gmail.com>
2026-04-24 23:50:04 +0800

    pencarian host ip

--- test.sh
+++ test.sh
@@ -1,19 +1,19 @@
 #!/bin/bash
 echo "Testing Gamify App..."
-# Tunggu 20 detik biar makin aman, Java/Nginx butuh waktu buat 'bangun'
-sleep 30 # MySQL butuh waktu buat "siap-siap"
+sleep 30
 
-HOST_IP=$(/sbin/ip route | awk '/default/ { print $3 }')
-echo "Detected Host IP: $HOST_IP"
+# MASUKKAN IP LXC KAMU SECARA MANUAL (Misal 192.168.1.16)
+TARGET_IP="192.168.1.16"
 
-# Ganti ke 8082 karena 8080 punyanya si Jenkins
-# Ganti localhost dengan IP Host/LXC kamu
-# Tambahkan /login.html di belakang IP
-status=$(curl -s -o /dev/null -w "%{http_code}" http://localhost:8082/login.html)
+echo "Targeting Host IP: $TARGET_IP"
+
+# Coba nembak ke login.html (pastikan nama filenya benar)
+status=$(curl -s -o /dev/null -w "%{http_code}" http://$TARGET_IP:8082/login.html)
+
 if [ $status -eq 200 ]; then
   echo "SUCCESS: App is rendering (HTTP 200)"
   exit 0
 else
-  echo "FAILED: App returned $status (Targeting Port 8082)"
+  echo "FAILED: App returned $status (Targeting Port 8082 di $TARGET_IP)"
   exit 1
 fi
