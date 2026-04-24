e4f864c725d15cf5ebc2db3417a0efa2373c7321
Tiffany <Galacthfansmo0nar45@gmail.com>
2026-04-24 22:13:33 +0800

    Update commit: Perbaikan pada file test.sh dibagian port

--- test.sh
+++ test.sh
@@ -1,12 +1,15 @@
 #!/bin/bash
 echo "Testing Gamify App..."
-sleep 15 # Tunggu container naik
-status=$(curl -s -o /dev/null -w "%{http_code}" http://localhost:8080)
+# Tunggu 20 detik biar makin aman, Java/Nginx butuh waktu buat 'bangun'
+sleep 20 
+
+# Ganti ke 8082 karena 8080 punyanya si Jenkins
+status=$(curl -s -o /dev/null -w "%{http_code}" http://localhost:8082)
 
 if [ $status -eq 200 ]; then
   echo "SUCCESS: App is rendering (HTTP 200)"
   exit 0
 else
-  echo "FAILED: App returned $status"
+  echo "FAILED: App returned $status (Targeting Port 8082)"
   exit 1
 fi
